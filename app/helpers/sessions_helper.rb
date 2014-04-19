49455be5876afceaa756cecec01535db4e613d38
Mar <rkx1901@gmail.com>
2014-04-18 23:05:09 -0500

    Add user  following

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,9 @@
 module SessionsHelper
+
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.hash(remember_token))
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
   end
 
@@ -15,15 +16,15 @@ module SessionsHelper
   end
 
   def current_user
-    remember_token = User.hash(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by_remember_token(remember_token)
   end
 
   def current_user?(user)
     user == current_user
   end
 
-   def signed_in_user
+  def signed_in_user
     unless signed_in?
       store_location
       redirect_to signin_url, notice: "Please sign in."
@@ -31,10 +32,8 @@ module SessionsHelper
   end
 
   def sign_out
-    current_user.update_attribute(:remember_token,
-                                  User.hash(User.new_remember_token))
+    current_user = nil
     cookies.delete(:remember_token)
-    self.current_user = nil
   end
 
   def redirect_back_or(default)
@@ -43,8 +42,6 @@ module SessionsHelper
   end
 
   def store_location
-    session[:return_to] = request.url if request.get?
+    session[:return_to] = request.url
   end
-
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,38 @@
 class User < ActiveRecord::Base
- has_many :microposts, dependent: :destroy
+  has_secure_password
+  has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id",
-                                   class_name: "Relationship",
-                                   dependent: :destroy
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
 
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   before_create :create_remember_token
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  after_validation { self.errors.messages.delete(:password_digest) }
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
 
-  def User.hash(token)
+  def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
 
   def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
-    relationships.find_by(followed_id: other_user.id)
+    relationships.find_by_followed_id(other_user.id)
   end
 
   def follow!(other_user)
@@ -38,14 +40,12 @@ class User < ActiveRecord::Base
   end
 
   def unfollow!(other_user)
-    relationships.find_by(followed_id: other_user.id).destroy
+    relationships.find_by_followed_id(other_user.id).destroy
   end
+
   private
 
     def create_remember_token
-      self.remember_token = User.hash(User.new_remember_token)
+      self.remember_token = User.encrypt(User.new_remember_token)
     end
-
-
-
 end
