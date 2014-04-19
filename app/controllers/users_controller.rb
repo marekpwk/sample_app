6b9b225107d240391237476542f95087661f831e
Mar <rkx1901@gmail.com>
2014-04-18 22:49:50 -0500

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_filter :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
-   def show
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
   end
@@ -12,18 +17,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
-    redirect_to users_url
-  end
-
-def create
-    @user = User.new(user_params)
+  def create
+    @user = User.new(params[:user])
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -31,38 +26,49 @@ def create
     else
       render 'new'
     end
-end
+  end
 
-def edit
-  @user = User.find(params[:id])
-end
+  def edit
+  end
 
-def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
+  def update
+    if @user.update_attributes(params[:user])
+      sign_in @user
       flash[:success] = "Profile updated"
       redirect_to @user
     else
       render 'edit'
     end
-end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
   private
 
-     def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-
-      # Before filters
-
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      redirect_to root_path unless current_user?(@user)
     end
 
+    def admin_user
+      redirect_to root_path unless current_user.admin?
+    end
 end
